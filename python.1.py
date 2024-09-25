07d04f32be09a2cdfeb418db564e18954f10e013
KandeVarshini <kvarshini159@gmail.com>
2024-09-25 19:15:53 +0530

    Update and rename python.1.py to advanced sub array problem.py

--- python.1.py
+++
@@ -1,9 +0,0 @@
-def remove_sql_specialists(people_list):
-    result=[]
-    for person in people_list:
-      if "sql" not in person[1]:
-         result.append(person)
-    return result
-new_hires=[("mark adams","sql analyst",4000),("leslie burton","hr specialist",2300),("dorothy castillo","ux designer",3100)]
-filtered_hires=remove_sql_specialists(new_hires)
-print(filtered_hires)
